a3b76b471f3510bd3e14e803d1fa1a627a2a1093
Kaell_O <mikaell.olondriz@benilde.edu.ph>
2020-08-07 00:26:10 +0800

    added scriptable objects for items

---
+++ Assets/Scripts/Interactables/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewItem", menuName = "Item")]
+public class Item : ScriptableObject
+{
+    public new string name;
+    public Sprite sprite;
+    public string job;
+    public string[] recipe;
+}
